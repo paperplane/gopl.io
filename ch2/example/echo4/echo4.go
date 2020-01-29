9d1c4ac9bd8b1dc2a431ee5c0c22e9320ef38aee
linilee <linilee@linilees-MBP.lan>
2020-01-29 23:44:08 +0800

    add ch2 example

---
+++ ch2/example/echo4/echo4.go
@@ -0,0 +1,72 @@
+package echo4
+
+import (
+	"flag"
+	"fmt"
+	"github.com/paperplane/gopl.io/ch2/example/tempconv"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+var p = f()
+
+func f() *int {
+	v := 1
+	return &v
+}
+
+func incr(p *int) int {
+	*p++
+	return *p
+}
+
+func newInt() *int {
+	return new(int)
+}
+
+func newInt2() *int {
+	var dummy int
+	return &dummy
+}
+
+func delta(new, old int) int {
+	return new - old
+}
+
+func main()  {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+
+	var x, y int
+	fmt.Println(&x == &y, &x == &x, &x == nil)
+
+	fmt.Println(f() == f())
+	fmt.Println(p == f())
+
+	v := 1
+	incr(&v)
+	fmt.Println(incr(&v))
+
+	p := newInt()
+	q := newInt2()
+
+	fmt.Println(p, q, p == q)
+
+	fmt.Printf("%g\n", tempconv.BoilingC-tempconv.FreezingC)
+	boilingF := tempconv.CToF(tempconv.BoilingC)
+	fmt.Printf("%g\n", boilingF - tempconv.CToF(tempconv.FreezingC))
+	fmt.Printf("%g\n", tempconv.FToC(boilingF) -tempconv.FreezingC)
+
+	var c tempconv.Celsius
+	var f tempconv.Fahrenheit
+	fmt.Println(c == 0)
+	fmt.Println(f >= 0)
+	fmt.Println(c == tempconv.Celsius(f))
+	fmt.Println(c.String())
+}
+
